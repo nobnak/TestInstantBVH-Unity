340db5fee904577691cb4164530603510c9e615d
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-03-13 16:17:12 +0900

    Init

---
+++ Assets/Examples/TestIntersection/TestIntersection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Recon.BoundingVolumes.Behaviour;
+using Recon.BoundingVolumes;
+
+public class TestIntersection : MonoBehaviour {
+    public Color color;
+    public ConvexBuilder[] convexes;
+
+	void OnDrawGizmos() {
+        IConvexPolyhedron ac, bc;
+
+        Gizmos.color = color;
+
+        foreach (var a in convexes) {
+            if (a == null || (ac = a.GetConvexPolyhedron()) == null)
+                continue;
+            
+            foreach (var b in convexes) {
+                if (b == null || (bc = b.GetConvexPolyhedron()) == null)
+                    continue;
+
+                if (ac.Intersect (bc))
+                    Gizmos.DrawLine (a.transform.position, b.transform.position);
+
+            }
+        }
+	}
+}
